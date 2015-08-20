fb97bbb2817852c0413df3668e4abb355eff2432
Rohit Sharma <skyrohithigh@gmail.com>
2015-08-20 15:25:04 +0530

    Added Constructor

--- app/src/main/java/com/bitshifter/wildfire/Country.java
+++ app/src/main/java/com/bitshifter/wildfire/Country.java
@@ -7,6 +7,11 @@ public class Country{
     private int id;
     private String name;
 
+    public Country(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
