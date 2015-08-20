c76bb8e8d7fbb8a85b1bde005606ce66979caada
Rohit Sharma <skyrohithigh@gmail.com>
2015-08-20 15:14:27 +0530

    Created Country Table in DB

---
+++ app/src/main/java/com/bitshifter/wildfire/Country.java
@@ -0,0 +1,25 @@
+package com.bitshifter.wildfire;
+
+/**
+ * Created by rohit on 20/8/15.
+ */
+public class Country{
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/bitshifter/wildfire/MyDBHandler.java
@@ -0,0 +1,37 @@
+package com.bitshifter.wildfire;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by rohit on 20/8/15.
+ */
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    private static final int DB_VERSION = 1;
+    private static final String DB_NAME ="wildfire.db";
+    private static final String TABLE_COUNTRY = "COUNTRY";
+    private static final String COUNTRY_COLUMN_CODE = "CODE";
+    private static final String COUNTRY_COLUMN_NAME = "NAME";
+
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DB_NAME, factory, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //Generating Country Table
+        String queryCountryTable = "CREATE TABLE "+TABLE_COUNTRY + " (" +
+                COUNTRY_COLUMN_CODE + " INTEGER PRIMARY KEY, "+
+                COUNTRY_COLUMN_NAME + " TEXT " +
+                ");";
+        db.execSQL(queryCountryTable);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //TODO WILL DECIDE LATER
+    }
+}
